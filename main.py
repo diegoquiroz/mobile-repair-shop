24dc18efb42987ddd390e52de9db2098bea498b3
Diego Quiroz <diego00aq@gmail.com>
2019-11-20 10:04:20 -0600

    Integración de What The Forms (WTF) e implementación de login form con un poco de Bootstrap

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'KEY'
+
+class LoginForm(FlaskForm):
+	username = StringField('Nombre de usuario', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	submit = SubmitField('Enviar')
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+	login_form = LoginForm()
+	context = {
+		'login_form': login_form
+	}
+	return render_template('form.html', **context)
