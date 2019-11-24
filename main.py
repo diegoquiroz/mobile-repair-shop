f3fe4e67303d20f8482b0d504d3e5e8c96c3f247
Diego Quiroz <diego00aq@gmail.com>
2019-11-24 09:36:23 -0600

    Index page with more style and Login form working

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.fields import StringField, PasswordField, SubmitField, SelectMultipleField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'KEY'
 
+
 class LoginForm(FlaskForm):
 	username = StringField('Nombre de usuario', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
-	submit = SubmitField('Enviar')
+	fields = SelectMultipleField('fields', choices=['1', '2', '3'])
+	submit = SubmitField('Log In')
 
 
 @app.route('/')
@@ -21,7 +23,19 @@ def index():
 @app.route('/login')
 def login():
 	login_form = LoginForm()
+
 	context = {
-		'login_form': login_form
+		'login_form': login_form,
 	}
-	return render_template('form.html', **context)
+
+	return render_template('login.html', **context)
+
+
+@app.route('/appointment')
+def schedule():
+	return render_template('schedule.html')
+
+
+@app.route('/device-status')
+def status():
+	return render_template('status.html')
