8fc12b97f2ce40d69614d3c1f2cc22fd48d034c6
Diego Quiroz <diego00aq@gmail.com>
2019-11-25 16:50:25 -0600

    First page of schedule form and new images static folder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectMultipleField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -9,9 +9,31 @@ app.config['SECRET_KEY'] = 'KEY'
 
 
 class LoginForm(FlaskForm):
-	username = StringField('Nombre de usuario', validators=[DataRequired()])
+	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	submit = SubmitField('Log In')
+
+
+class PhoneForm(FlaskForm):
+	brand = SelectMultipleField('Brand', choices=[
+		('1', 'Apple'),
+		('2', 'Huawei'),
+		('3', 'Samsung'),
+		('4', 'LG'),
+		('5', 'Xiaomi'),
+		('6', 'Nokia')
+	])
+	model = SelectMultipleField('Model', choices=[
+		('1', 'iPhone 11 Pro'),
+		('2', 'iPhone 11 Pro Max'),
+		('3', 'iPhone 11'),
+		('4', 'iPhone XS'),
+		('5', 'iPhone XS Max'),
+		('6', 'iPhone XR'),
+		('7', 'iPhone X'),
+		('8', 'iPhone 8 Plus'),
+		('9', 'iPhone 8')
+	])
 
 
 @app.route('/')
@@ -21,7 +43,13 @@ def index():
 
 @app.route('/appointment')
 def schedule():
-	return render_template('schedule.html')
+	form = PhoneForm()
+
+	context = {
+		'form': form,
+	}
+
+	return render_template('schedule.html', **context)
 
 
 @app.route('/dashboard')
@@ -50,8 +78,7 @@ def login():
 
 	context = {
 		'login_form': login_form,
-		'username': username	username = session.get('username')
-
+		'username': username
 	}
 
 	if username:
